bc3dcbca7f5b280d2a0348da82fa846c55058d68
FutaAlice <1611271600@qq.com>
2017-10-22 19:19:09 +0800

    Test feature-color

--- test/fhcore_test.cpp
+++ test/fhcore_test.cpp
@@ -4,10 +4,14 @@
 #include <logger.h>
 #include "fhcore_test.h"
 using namespace std;
+using namespace logger;
 
 int test_fhcore_all()
 {
     if (0 != test_fhcore_logger())
+        return -1;
+
+    if (0 != test_fhcore_color())
         return -1;
 
     return 0;
@@ -15,10 +19,46 @@ int test_fhcore_all()
 
 int test_fhcore_logger()
 {
-    using namespace logger;
-    
-    debug() << string(__func__);
-    record() << string(__func__);
+    debug(Level::Info) << __func__;
+    record(Level::Info) << __func__;
 
     return 0;
 }
+
+#include <color.h>
+int test_fhcore_color()
+{
+    using namespace color;
+
+    Color r = Color::Red, b = Color::Blue;
+    auto not_r = !r;
+    auto not_b = !b;
+
+    auto func_output = [](auto a) {
+        debug(Level::Info) << "Type: "<<typeid(a).name() << ", "
+            << "Value: " << a;
+    };
+    func_output(r);
+    func_output(not_r);
+    func_output(b);
+    func_output(not_b);
+
+    if (typeid(Color).hash_code() != typeid(r).hash_code() ||
+        typeid(Color).hash_code() != typeid(r).hash_code() ||
+        typeid(Color).hash_code() != typeid(r).hash_code() ||
+        typeid(Color).hash_code() != typeid(r).hash_code())
+    {
+        return -1;
+    }
+
+    if (r != not_b || b != not_r)
+    {
+        return -1;
+    }
+
+    const char *arr[2] = { "Red", "Blue" };
+    auto a_r = arr[&r];
+
+    return 0;
+}
+
--- test/fhcore_test.h
+++ test/fhcore_test.h
@@ -3,3 +3,4 @@
 int test_fhcore_all();
 
 int test_fhcore_logger();
+int test_fhcore_color();
