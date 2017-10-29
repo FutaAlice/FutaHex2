0c037a005d6bd47090fa00c834e033183403d737
FutaAlice <1611271600@qq.com>
2017-10-29 13:12:52 +0800

    Add value Color::Empty to enum.

--- src/fhcore/color.h
+++ src/fhcore/color.h
@@ -1,20 +1,32 @@
 #pragma once
+#include <cassert>
 
 namespace color
 {
-using color_t = unsigned char;
+using color_t = char;
 
 enum class Color :color_t
 {
+    Empty = -1,
     Red = 0,
-    Blue,
+    Blue = 1,
 };
 
-constexpr Color operator!(const Color color)
+// Get opposite color.
+#if defined(DEBUG) || defined(_DEBUG)
+Color
+#else if
+constexpr Color
+#endif
+operator!(const Color color)
 {
+#if defined(DEBUG) || defined(_DEBUG)
+    assert(color != Color::Empty);
+#endif
     return static_cast<Color>(!static_cast<color_t>(color));
 }
 
+// Convert to color_t.
 constexpr color_t operator&(const Color color)
 {
     return static_cast<color_t>(color);
