f717def6cdfaefc7f82f7c33cf8f4444515ef951
FutaAlice <1611271600@qq.com>
2018-03-11 15:10:59 +0800

    add engine interface

---
+++ src/fhcore/iengine.cpp
@@ -0,0 +1,57 @@
+#include <cassert>
+#include "iengine.h"
+
+using namespace std;
+using namespace board;
+
+namespace engine
+{
+
+IEngine::~IEngine()
+{
+    _future.wait();
+}
+
+void IEngine::configure(EngineCfg cfg) noexcept
+{
+    assert(cfg.pBoard && Color::Empty != cfg.colorAI);
+
+    lock();
+    delete pBoard;
+    pBoard = cfg.pBoard->copy();
+    colorAI = cfg.colorAI;
+    unlock();
+}
+
+void IEngine::compute(FUNC_CB_AIMOVE cb, void *opaque) noexcept
+{
+    assert(cb);
+
+    _future = async(launch::async, [this, cb, opaque] {
+        lock();
+        auto result = calc_ai_move_sync();
+        unlock();
+        cb(result, opaque);
+    });
+}
+
+void IEngine::compute_sync(position::pos_t & result) noexcept
+{
+    lock();
+    result = calc_ai_move_sync();
+    unlock();
+}
+
+void IEngine::lock()
+{
+    bool is_thinking = !_lock.try_lock();
+    if (is_thinking)
+        throw;
+}
+
+void IEngine::unlock()
+{
+    _lock.unlock();
+}
+
+}
