3fa5a0603aa2f91f1e1e0bcd700fc05b28a6ebdc
WangXB <1611271600@qq.com>
2018-03-17 10:28:27 +0800

    refactor and format code

--- src/fhcore/disjointset.h
+++ src/fhcore/disjointset.h
@@ -19,14 +19,14 @@ public:
     static IDisjointSet * create(board::IBoard *pBoard);
 
 public:
-    virtual color::Color get(int index) = 0;
+    virtual Color get(int index) = 0;
     virtual void set(int index) = 0;
-    virtual color::Color & operator[](int index) = 0;
+    virtual Color & operator[](int index) = 0;
     virtual void revert() = 0;
     virtual void ufinit() = 0;
     virtual bool check_after_set(int center_index, const Color color) = 0;
 public:
-    color::Color color_to_move() { return static_cast<Color>(rounds & 1); }
+    Color color_to_move() { return static_cast<Color>(rounds & 1); }
 protected:
     size_t rounds;
 };
@@ -35,15 +35,16 @@ template<typename Test, coord_t size>
 class DisjointSetT : public IDisjointSet
 {
     friend Test;
+    using Position = PositionT<size>;
 public:
     DisjointSetT() = default;
     DisjointSetT(board::IBoard *pBoard);
 public:
     static IDisjointSet * create(board::IBoard *pBoard);
 public:
-    virtual color::Color get(int index);
+    virtual Color get(int index);
     virtual void set(int index);
-    virtual color::Color & operator[](int index);
+    virtual Color & operator[](int index);
     virtual void revert();
     virtual void ufinit();
     virtual bool check_after_set(int center_index, const Color color);
@@ -53,9 +54,10 @@ private:
     void merge(int center_index, Color color);
 
 public:
-    color::Color status[size * size];
+    Color status[size * size];
 
 private:
+    const Position & _pos{ Position::instance() };
     int fa[2][size * size + 2];
     board::IBoard *pBoard;
 };
